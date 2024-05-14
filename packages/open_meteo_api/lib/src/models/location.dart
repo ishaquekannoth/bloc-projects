76ce2d0a0f528e880b8f8ea834d07783bf96ecb0
Ishaque <ishaque@mobikasa.com>
2024-05-14 17:58:25 +0530

    basic set up done

---
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'location.g.dart';
+@JsonSerializable()
+class Location {
+  const Location({
+    required this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final int id;
+  final String name;
+  final double latitude;
+  final double longitude;
+}
